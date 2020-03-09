51e8d78646b937b0e342201d1366473f51e1505a
Steven Shaginyan <steven.shaginyan@heroku.com>
2020-03-09 12:19:59 -0700

    Starting

--- server.js
+++ server.js
@@ -3,14 +3,10 @@ process.env.DATABASE_URL += '?ssl=true';
 const faker = require('faker');
 const uuidv1 = require('uuid/v1');
 const { Kafka } = require('kafkajs');
-const knex = require('knex')({
-  client: 'postgres',
-  connection: process.env.DATABASE_URL
-});
 
 const kafka = new Kafka({
   clientId: 'crowdstrike',
-  brokers: ['ec2-18-209-198-121.compute-1.amazonaws.com:9096', 'ec2-3-211-8-44.compute-1.amazonaws.com:9096', 'ec2-3-209-193-170.compute-1.amazonaws.com:9096'],
+  brokers: ['ec2-52-206-175-15.compute-1.amazonaws.com:9096', 'ec2-3-232-196-179.compute-1.amazonaws.com:9096' ,'ec2-34-228-251-107.compute-1.amazonaws.com:9096'],
   ssl: {
     rejectUnauthorized: false,
     ca: [process.env.KAFKA_TRUSTED_CERT],
@@ -47,7 +43,7 @@ const consumer = kafka.consumer({ groupId: 'crowdstrike' });
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       const data = JSON.parse(message.value.toString());
-      await knex.withSchema('salesforce').table('crowdstrike__c').insert(data);
+      console.log(data);
     }
   })
 })();
