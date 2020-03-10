f1760de2d30b58ce5c0e70eff71c3c927b502bc3
Steven Shaginyan <steven.shaginyan@heroku.com>
2020-03-10 13:48:25 -0700

    Starting

--- server.js
+++ server.js
@@ -69,15 +69,15 @@ const consumer = kafka.consumer({ groupId: KAFKA_TOPIC_CDC });
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       const data = JSON.parse(message.value.toString());
-      connections.forEach(socket => {
+      connections.forEach( async socket => {
         socket.emit('message', data);
+        const dataString = JSON.stringify(data);
+        const dataClone = JSON.parse(dataString);
+        delete dataClone.payload.ChangeEventHeader;
+        await knex.withSchema('public').table('accounts').insert({ cdc: JSON.stringify(dataClone.payload) });
       });
-      // const dataString = JSON.stringify(data);
-      // const dataClone = JSON.parse(dataString);
-      delete data.payload.ChangeEventHeader;
-      await knex.withSchema('public').table('accounts').insert({ cdc: JSON.stringify(data.payload) });
     }
-  });
+  })
 })();
 
 server.listen(process.env.PORT || 8080);
